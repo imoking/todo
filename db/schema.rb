00fd152e8f347db2dcd9620fe0ada2a8f044c4a2
EC2 Default User <ec2-user@ip-172-31-29-87.us-east-2.compute.internal>
2018-10-29 07:56:35 +0000

    css

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_070542) do
+ActiveRecord::Schema.define(version: 2018_10_29_074113) do
 
   create_table "todos", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "titles"
   end
 
   create_table "users", force: :cascade do |t|
